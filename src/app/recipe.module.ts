0c0d3ed6abbc5ee165ea4eaae5913cf0d89a4972
sarz2 <sara.abou@chasacademy.se>
2022-04-04 11:45:41 +0200

    create component for recipe-details & recipe-list

--- src/app/recipe.module.ts
+++ src/app/recipe.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeRoutingModule } from './recipe/recipe-routing-module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 
 
 @NgModule({
   declarations: [
-    RecipeComponent
+    RecipeComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent
   ],
   imports: [
     CommonModule,
