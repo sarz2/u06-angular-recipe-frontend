a5860725b174caf0ffe2304f343776d7695245c6
sarz2 <sara.abou@chasacademy.se>
2022-04-09 21:19:33 +0200

    some clenup

--- src/app/recipe.module.ts
+++ src/app/recipe.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeRoutingModule } from './recipe/recipe-routing-module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 
 
 @NgModule({
   declarations: [
-    RecipeComponent,
-    RecipeDetailsComponent,
-    RecipeListComponent
+    RecipeDetailsComponent
   ],
   imports: [
     CommonModule,
--- src/app/recipe/recipe-routing-module.ts
+++ src/app/recipe/recipe-routing-module.ts
@@ -2,12 +2,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { IndexComponent } from "./index/index.component";
 import { RecipeDetailsComponent } from "../recipe-details/recipe-details.component";
+import { LoginComponent } from "../login/login.component";
 
 const routes: Routes = [
     { path: '', redirectTo: 'recipe', pathMatch: 'full' },
     { path: 'recipe', redirectTo: 'recipe/index', pathMatch: 'full' },
     { path: 'recipe/index', component: IndexComponent },
-    { path: 'detail/:id', component: RecipeDetailsComponent }
+    { path: 'detail/:id', component: RecipeDetailsComponent },
+
 
 ];
 
